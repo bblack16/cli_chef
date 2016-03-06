33f57067104b5b905ae511863cbf4e6451079afe
Brandon <d2sm10@hotmail.com>
2016-03-06 13:52:13 -0700

    Removed from BBLib namespace.

--- lib/cabinet.rb
+++ lib/cabinet.rb
@@ -1,66 +1,62 @@
 require_relative 'ingredient'
 
-module BBLib
+module CLIChef
 
-  module CLIChef
+  class Cabinet
+    attr_reader :ingredients
 
-    class Cabinet
-      attr_reader :ingredients
+    def initialize
+      @ingredients = Hash.new
+    end
 
-      def initialize
-        @ingredients = Hash.new
-      end
+    def add_ingredient ingredient
+      raise "Invalid type passed as ingredientument: #{ingredient.class}." unless ingredient.is_a? Ingredient
+      @ingredients[ingredient.name] = ingredient
+    end
 
-      def add_ingredient ingredient
-        raise "Invalid type passed as ingredientument: #{ingredient.class}." unless ingredient.is_a? Ingredient
-        @ingredients[ingredient.name] = ingredient
-      end
+    def delete_ingredient ingredient
+      @ingredients.delete (ingredient.is_a?(Symbol) ? ingredient : ingredient.name)
+    end
 
-      def delete_ingredient ingredient
-        @ingredients.delete (ingredient.is_a?(Symbol) ? ingredient : ingredient.name)
-      end
-
-      def ingredient_string hash
-        str = ''
-        hash.each do |k, v|
-          ingredient = nil
-          if @ingredients.include? k
-            ingredient = @ingredients[k]
-          else
-            ingredient = @ingredients.values.find{ |val| val.aliases.include?(k) }
-          end
-          if ingredient
-            [v].flatten.each do |vl|
-              next unless vl != false
-              if vl == true
-                ingredient.value = nil
-              else
-                ingredient.value = vl
-              end
-              str+= ingredient.to_s + ' '
+    def ingredient_string hash
+      str = ''
+      hash.each do |k, v|
+        ingredient = nil
+        if @ingredients.include? k
+          ingredient = @ingredients[k]
+        else
+          ingredient = @ingredients.values.find{ |val| val.aliases.include?(k) }
+        end
+        if ingredient
+          [v].flatten.each do |vl|
+            next unless vl != false
+            if vl == true
+              ingredient.value = nil
+            else
+              ingredient.value = vl
             end
+            str+= ingredient.to_s + ' '
           end
         end
-        str.strip
       end
+      str.strip
+    end
 
-      def from_csv csv
-        from_sv csv, ","
+    def from_csv csv
+      from_sv csv, ","
+    end
+
+    def from_tsv tsv
+      from_sv tsv, "\t"
+    end
+
+    def from_sv sv, delim
+      lines = sv.split("\n")
+      headers = lines.first.split(delim).map{ |m| m.downcase.to_sym }
+      lines[1..-1].each do |l|
+        h = headers.zip(l.split("\t")).to_h
+        add_ingredient(CLIChef::Ingredient.new h[:name], description:h[:description], default:(h[:default] == 'nil' ? nil : h[:default]), flag:(h[:flag] == 'nil' ? nil : h[:flag]), value:h[:value], spacer:h[:spacer], encapsulator:h[:encapsulator], aliases:h[:aliases].to_s.split('|'), allowed_values:h.include?(:allowed_values) ? h[:allowed_values].to_s.split('|').map{ |a| eval(a) } : Object)
       end
-
-      def from_tsv tsv
-        from_sv tsv, "\t"
-      end
-
-      def from_sv sv, delim
-        lines = sv.split("\n")
-        headers = lines.first.split(delim).map{ |m| m.downcase.to_sym }
-        lines[1..-1].each do |l|
-          h = headers.zip(l.split("\t")).to_h
-          add_ingredient(BBLib::CLIChef::Ingredient.new h[:name], description:h[:description], default:(h[:default] == 'nil' ? nil : h[:default]), flag:(h[:flag] == 'nil' ? nil : h[:flag]), value:h[:value], spacer:h[:spacer], encapsulator:h[:encapsulator], aliases:h[:aliases].to_s.split('|'), allowed_values:h.include?(:allowed_values) ? h[:allowed_values].to_s.split('|').map{ |a| eval(a) } : Object)
-        end
-      end
-
     end
 
   end
--- lib/recipe_book.rb
+++ lib/recipe_book.rb
@@ -1,33 +1,29 @@
 require_relative 'recipe'
 
-module BBLib
+module CLIChef
 
-  module CLIChef
+  class RecipeBook
+    attr_reader :recipes
 
-    class RecipeBook
-      attr_reader :recipes
+    def initialize
+      @recipes = Hash.new
+    end
 
-      def initialize
-        @recipes = Hash.new
-      end
+    def add_recipe recipe
+      raise "Invalid class passed for recipe (#{recipe.class}). Must be a CLIChef::Recipe." unless recipe.is_a? CLIChef::Recipe
+      @recipes[recipe.name] = recipe
+    end
 
-      def add_recipe recipe
-        raise "Invalid class passed for recipe (#{recipe.class}). Must be a BBLib::CLIChef::Recipe." unless recipe.is_a? BBLib::CLIChef::Recipe
-        @recipes[recipe.name] = recipe
-      end
+    def remove_recipe name
+      @recipes.delete recipe
+    end
 
-      def remove_recipe name
-        @recipes.delete recipe
-      end
+    def [] recipe
+      @recipes[recipe]
+    end
 
-      def [] recipe
-        @recipes[recipe]
-      end
-
-      def include? name
-        @recipes.include? name
-      end
-
+    def include? name
+      @recipes.include? name
     end
 
   end
